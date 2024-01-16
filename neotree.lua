95462dc45ca147f54a90b1ecf40338fa788a1de5
Connway <nicholas.lacroix@gmail.com>
2024-01-16 01:05:26 -0500

    Adding telescope code for neotree. Need new keybinds.

--- neotree.lua
+++ neotree.lua
@@ -1,3 +1,25 @@
+local function GetTelescopeOpts(state, path)
+  return {
+    cwd = path,
+    search_dirs = { path },
+    attach_mappings = function (prompt_bufnr, map)
+      local actions = require "telescope.actions"
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local action_state = require "telescope.actions.state"
+        local selection = action_state.get_selected_entry()
+        local filename = selection.filename
+        if (filename == nil) then
+          filename = selection[1]
+        end
+        -- any way to open the file without triggering auto-close event of neo-tree?
+        require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
+      end)
+      return true
+    end
+  }
+end
+
 require("neo-tree").setup({
    source_selector = {
         winbar = true,
@@ -41,6 +63,15 @@ require("neo-tree").setup({
 
     close_if_last_window = true,
 
+    filesystem = {
+        window = {
+            mappings = {
+                --["tf"] = "telescope_find",
+                --["tg"] = "telescope_grep",
+            }
+        }
+    },
+
     event_handlers = {
         {
             event = "file_opened",
@@ -48,7 +79,20 @@ require("neo-tree").setup({
                 require("neo-tree.command").execute({ action = "close" })
             end
         }
-    }
+    },
+
+    commands = {
+      telescope_find = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        require('telescope.builtin').find_files(GetTelescopeOpts(state, path))
+      end,
+      telescope_grep = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        require('telescope.builtin').live_grep(GetTelescopeOpts(state, path))
+      end,
+    },
 })
 
 vim.keymap.set(
