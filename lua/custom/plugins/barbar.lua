96a8f18e286b37924ddf449a84f05ca8c1698f7f
Connway <nicholas.lacroix@gmail.com>
2024-12-29 16:51:46 -0500

    Uncommenting out some barbar keybinds for buffer ordering. Adding alternate keybinds for buffer moving to make it work in terminal windows.

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -13,10 +13,12 @@ return {
     vim.keymap.set('n', '<C->>', '<Cmd>BufferMoveNext<CR>', { silent = true })
     vim.keymap.set('n', '<C-c>', '<Cmd>BufferClose<CR>', { silent = true })
     vim.keymap.set('n', '<C-p>', '<Cmd>BufferPick<CR>', { silent = true })
+    vim.keymap.set('n', '<A-,>', '<Cmd>BufferMovePrevious<CR>', { silent = true })
+    vim.keymap.set('n', '<A-.>', '<Cmd>BufferMoveNext<CR>', { silent = true })
 
-    -- vim.keymap.set('n', '<Space>bn', '<Cmd>BufferOrderByBufferNumber<CR>', { desc = 'Order [b]uffers by [n]umber.', silent = true })
-    -- vim.keymap.set('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', { desc = 'Order [b]uffers by [d]irectory.', silent = true })
-    -- vim.keymap.set('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', { desc = 'Order [b]uffers by [l]anguage.', silent = true })
-    -- vim.keymap.set('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', { desc = 'Order [b]uffers by [w]indow id.', silent = true })
+    vim.keymap.set('n', '<Space>bn', '<Cmd>BufferOrderByBufferNumber<CR>', { desc = 'Order [b]uffers by [n]umber.', silent = true })
+    vim.keymap.set('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', { desc = 'Order [b]uffers by [d]irectory.', silent = true })
+    vim.keymap.set('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', { desc = 'Order [b]uffers by [l]anguage.', silent = true })
+    vim.keymap.set('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', { desc = 'Order [b]uffers by [w]indow id.', silent = true })
   end,
 }
