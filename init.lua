af13044d2bfd08bf25db4e06e83029e5b8059d95
Connway <nicholas.lacroix@gmail.com>
2025-01-01 21:32:10 -0500

    Adding meson files to format on save ignore list.

--- init.lua
+++ init.lua
@@ -715,8 +715,9 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, zig = true, zon = true }
+        local disable_filetypes = { c = true, cpp = true, zig = true, zon = true, meson = true }
         local lsp_format_opt
+
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
         else
