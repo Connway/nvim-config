98fac2f58a37a176caeaa65aea631a84330a8167
Connway <nicholas.lacroix@gmail.com>
2025-03-28 19:11:29 -0400

    Switching theme to tokyonight.

--- init.lua
+++ init.lua
@@ -168,7 +168,6 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
-
 if vim.loop.os_uname().sysname == 'Darwin' then
   vim.opt.guifont = 'MesloLGL Nerd Font Mono:h12'
 else
@@ -922,13 +921,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'navarasu/onedark.nvim',
-    -- 'folke/tokyonight.nvim',
+    -- 'navarasu/onedark.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      --require('tokyonight').setup {
-      require('onedark').setup {
+      require('tokyonight').setup {
+        -- require('onedark').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
@@ -937,8 +936,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'onedark'
     end,
   },
 
