a81369bf4121517f697b7767a6ef783f272d6f03
Connway <nicholas.lacroix@gmail.com>
2025-07-22 10:31:24 -0400

    Disabling ouroboros.

--- lua/custom/plugins/ouroboros.lua
+++ lua/custom/plugins/ouroboros.lua
@@ -1,17 +1,18 @@
-return {
-  'jakemason/ouroboros',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    vim.keymap.set('n', '<C-e>', '<Cmd>Ouroboros<CR>')
-
-    require('ouroboros').setup {
-      extension_preferences_table = {
-        c = { h = 2, hpp = 1 },
-        h = { c = 2, cpp = 1 },
-        cpp = { hpp = 2, h = 1 },
-        hpp = { cpp = 1, c = 2 },
-      },
-      switch_to_open_pane_if_possible = true,
-    }
-  end,
-}
+return {}
+-- return {
+--   'jakemason/ouroboros',
+--   dependencies = { 'nvim-lua/plenary.nvim' },
+--   config = function()
+--     vim.keymap.set('n', '<C-e>', '<Cmd>Ouroboros<CR>')
+--
+--     require('ouroboros').setup {
+--       extension_preferences_table = {
+--         c = { h = 2, hpp = 1 },
+--         h = { c = 2, cpp = 1 },
+--         cpp = { hpp = 2, h = 1 },
+--         hpp = { cpp = 1, c = 2 },
+--       },
+--       switch_to_open_pane_if_possible = true,
+--     }
+--   end,
+-- }
